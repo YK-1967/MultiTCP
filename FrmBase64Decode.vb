aab8e87cfdb934a5d56a4d0866ef65514695a1c6
YK-1967 <yk.lin1967@gmail.com>
2020-02-25 14:48:13 +0800

    Add Base64 Code

---
+++ FrmBase64Decode.vb
@@ -0,0 +1,9 @@
+﻿Public Class FrmBase64Decode
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox2.Text = Base64Decode(TextBox1.Text)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Close()
+    End Sub
+End Class
